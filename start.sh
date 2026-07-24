ea228c9a3b926cb93749c54537aa3701a4a20e65
Guillem Burnlees <guillem.burnlees@grupodynasoft.com>
2026-07-24 11:47:12 +0200

    ajustes

--- start.sh
+++ start.sh
@@ -15,7 +15,17 @@ cleanup() {
 }
 trap cleanup EXIT INT TERM
 
-AGENT_VERSION=$(curl -fsSL https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | jq -r '.tag_name' | sed 's/^v//') curl -fsSL "https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz" | tar -xz
+AGENT_VERSION=$(curl -fsSL https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | jq -r '.tag_name' | sed 's/^v//')
+
+PKG="vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz"
+URL1="https://download.agent.dev.azure.com/agent/${AGENT_VERSION}/${PKG}"
+URL2="https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/${PKG}"
+
+curl -fL --retry 5 --retry-delay 3 -o "/azp/${PKG}" "$URL1" || \
+curl -fL --retry 5 --retry-delay 3 -o "/azp/${PKG}" "$URL2"
+
+tar -xzf "/azp/${PKG}" -C /azp
+rm -f "/azp/${PKG}"
 
 /azp/bin/installdependencies.sh
 
